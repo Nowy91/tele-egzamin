b6506e80735926e201890a54acfed55a6f24437a
mikipak <stepczak.mikolaj@gmail.com>
2013-12-10 17:44:44 +0100

    Poprawa wyświetlania obrazków i danych w tabelach

--- public/javascripts/views/check/checked_item.js
+++ public/javascripts/views/check/checked_item.js
@@ -2,14 +2,19 @@ App.Views.CheckedTokenItem = Marionette.CompositeView.extend({
 
     tagName: 'tr',
 
-    render: function() {
+    render: function () {
         var context = this.model.toJSON();
-        context['formattedExecutedDate'] = context['executedDate'].substring(0,10);
+        context['formattedExecutedDate'] = context['executedDate'].substring(0, 10);
+        var newPoints = context['reachedPoints'].replace(/0*$/, "");
+        if (newPoints.indexOf('.') == (newPoints.length - 1))
+            context['formattedReachedPoints'] = newPoints.substring(0, (newPoints.length - 1));
+        else
+            context['formattedReachedPoints'] = newPoints;
         var html = this.template(context);
         $(this.el).html(html);
     },
 
-    initialize: function() {
+    initialize: function () {
         this.template = App.Templates.get('checked_item');
     }
 
